0f84ee1f68d697efefceecbcbbb6db03cd78b8bf
Yufan Lu <bruce.yufanlu@gmail.com>
2020-05-21 00:34:55 -0400

    try emacs

---
+++ emacs/configuration.el
@@ -0,0 +1,57 @@
+;; fancy lambdas
+(global-prettify-symbols-mode t)
+
+
+(use-package base16-theme
+  :config
+  (load-theme 'base16-gruvbox-dark-medium t))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package auto-compile
+  :config (auto-compile-on-load-mode))
+
+(setq load-prefer-newer t)
+
+(use-package company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(use-package lsp-mode)
+(use-package lsp-python-ms
+  :after lsp-mode
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))  ; or lsp-deferred
+
+(use-package evil
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-escape
+  :config
+  (evil-escape-mode)
+  (setq-default evil-escape-delay 0.2)
+  (setq-default evil-escape-key-sequence "jk"))
+
+;; (use-package linum-relative
+;;   :commands (linum-relative-toggle linum-relative-on)
+;;   :config
+;;   (linum-on)
+;;   (linum-relative-on)
+;;   (setq linum-relative-backend 'display-line-numbers-mode))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+ 
+(setq org-src-fontify-natively t)
+
+(use-package org-bullets
+  :init
+  (add-hook 'org-mode-hook 'org-bullets-mode))
---
+++ emacs/elpa/bind-key-20191110.416/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bind-key" "20191110.416" "A simple way to manage personal keybindings" 'nil :commit "d2640fec376a8458a669e7526e63e5870d875118" :keywords '("keys" "keybinding" "config" "dotemacs") :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :url "https://github.com/jwiegley/use-package")
